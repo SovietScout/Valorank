70e8fcb142bfc732da70bc61e89305560f956bdf
SovietScout <sovietscout@protonmail.com>
2022-07-31 01:17:42 +0530

    Added logger

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 )
 
 func main() {
+	f, err := tea.LogToFile("debug.log", "DEBUG: ")
+	if err != nil {
+		log.Fatalln("Couldn't initialise logger")
+	}
+	defer f.Close()
+
 	termenv.SetWindowTitle(content.NAME + " by " + content.AUTHOR)
 
 	p := tea.NewProgram(valorank.NewModel(), tea.WithAltScreen())
